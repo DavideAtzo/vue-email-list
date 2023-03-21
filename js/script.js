36f20b248e0f2bc67b84a390567b77f4dbcca192
DavideAtzo <atzo9477@gmail.com>
2023-03-21 16:20:56 +0100

    creazione generazione mail e stampati

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 'use strict';
-// alert('funziona');
 
 const { createApp } = Vue
-//option api
 createApp({
     data() {
         return {
+           email:[],
            
         }
     },
+    methods: {
+    },
+    beforeMount() {
+        for(let i = 1; i <= 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                this.email.push(response.data.response)
+            });
+        }
+    },
+    
 }
 ).mount('#app');
+
