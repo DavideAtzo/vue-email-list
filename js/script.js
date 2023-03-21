3453303df1573a02bffe48adeb1c9debc05bbef9
DavideAtzo <atzo9477@gmail.com>
2023-03-21 16:24:00 +0100

    miglioramento visualizzazione mail

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-           email:[],
+           emails:[],
            
         }
     },
@@ -14,7 +14,7 @@ createApp({
         for(let i = 1; i <= 10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
-                this.email.push(response.data.response)
+                this.emails.push(response.data.response)
             });
         }
     },
